6e6b31e032c5c720724d1311dd5a8dbc7b88efe5
z-dian <zhangdian0609@163.com>
2020-09-08 21:04:54 +0800

    剑指offer35,36

---
+++ leecode/src/牛客/剑指offer/JZ36_Solution.java
@@ -0,0 +1,22 @@
+package 牛客.剑指offer;
+
+import java.util.HashMap;
+
+public class JZ36_Solution {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode current1 = pHead1;
+        ListNode current2 = pHead2;
+
+        HashMap<ListNode, Integer> hashMap = new HashMap<>();
+        while (current1 != null) {
+            hashMap.put(current1, null);
+            current1 = current1.next;
+        }
+        while (current2 != null) {
+            if (hashMap.containsKey(current2))
+                return current2;
+            current2 = current2.next;
+        }
+        return null;
+    }
+}
